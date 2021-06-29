b80e00c49c62804de91f76dd1b5a3c0c48643f39
Rikuba <rikuba@live.jp>
2021-06-29 16:31:13 +0900

    初回表示用のデータを用意する

--- src/store.js
+++ src/store.js
@@ -1,5 +1,24 @@
 import { storage } from './storage'
 
+const initialState = {
+  memos: [
+    {
+      id: '0',
+      title: 'VueMemoの使い方',
+      content: [
+        'VueMemoの使い方',
+        '',
+        '一行目がメモのタイトルになります。',
+        '',
+        '「メモを追加」を押すと新規メモが追加されます。',
+        '「編集」ボタンを押すと表示中のメモの内容が保存されます。',
+        '「削除」ボタンを押すと表示中のメモが削除されます。'
+      ].join('\n')
+    }
+  ],
+  nextMemoId: 1
+}
+
 export const store = {
   storage,
 
@@ -9,7 +28,7 @@ export const store = {
   },
 
   load () {
-    const state = this.storage.load()
+    const state = this.storage.load() || initialState
     Object.assign(this.state, state)
   },
 
